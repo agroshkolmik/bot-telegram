8e89465a7b8addbb00960b92ad37abf55cfed633
Student <aswdasfa@email.com>
2024-05-27 13:34:58 +0300

    esgfseg

--- main.py
+++ main.py
@@ -2,14 +2,25 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 import asyncio
 
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters.state import StateFilter
+from UserStates import UserStates
+
 from config import TOKEN
 
+storage=MemoryStorage()
 bot=Bot(token=TOKEN)
-dp=Dispatcher()
+dp=Dispatcher(storage=storage)
 
 @dp.message(Command("start"))
-async def send_welcome(message: types.Message):
-    await bot.send_message(message.chat.id, "ku trup")
+async def start(message: types.Message, state:FSMContext):
+    await message.answer("ku trup")
+    await state.set_state(UserStates.BASE)
+
+@dp.message(Command("test"), StateFilter(UserStates.BASE))
+async def start(message: types.Message):
+    await message.answer("u are inBAse")
 
 async def main():
     await dp.start_polling(bot)
