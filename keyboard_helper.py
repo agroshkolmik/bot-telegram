4abfffe6b8209d3ea8c75ea7405659978fd63a3d
Student <aswdasfa@email.com>
2024-05-27 12:38:53 +0300

    esgfseg

---
+++ keyboard_helper.py
@@ -0,0 +1,8 @@
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.types import KeyboardButton
+def get_keyboard(buttons):
+    kb=[]
+    for button in buttons:
+        kb.append([KeyboardButton(text=button)])
+    reply_markup=ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    return reply_markup
