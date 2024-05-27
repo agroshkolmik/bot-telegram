53cb9cb255f8b8fea2c691a3036d98d50ca738d0
Student <aswdasfa@email.com>
2024-05-27 14:30:21 +0300

    esgfseg

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.filters.state import StateFilter
 from UserStates import UserStates
+from aiogram import F
 from keyboard_helper import keyboards
-
+import pari_service as ps
 from config import TOKEN
 
 storage=MemoryStorage()
@@ -20,10 +21,17 @@ async def start(message: types.Message, state:FSMContext):
     await message.answer("ku trup", reply_markup=kb)
     await state.set_state(UserStates.BASE)
 
-@dp.message(Command("test"), StateFilter(UserStates.BASE))
-async def start(message: types.Message):
-    await message.answer("u are inBAse")
-
+@dp.message(F.text=="My paris", StateFilter(UserStates.BASE))
+async def My_paris(message: types.Message):
+    text="your paris"
+    paris=ps.get_paris(message.from_user.id)
+    for pari in paris:
+        text+="\n"+pari
+    await message.answer(text)
+@dp.message(F.text=="Create pari", StateFilter(UserStates.BASE))
+async def create_pari(message: types.Message):
+    text=ps.add_pari(message.from_user.id,message.text)
+    await message.answer(text)
 async def main():
     await dp.start_polling(bot)
 
---
+++ pari_service.py
@@ -0,0 +1,5 @@
+def add_pari(user_id,pari):
+    message="pari added \""+pari+"\" for " + str(user_id)
+    return message
+def get_paris(user_id):
+    return ["Test1","Test2","Test1"]
