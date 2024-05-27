8f10c12b47528b48160d4045c485c6289cef8599
Student <aswdasfa@email.com>
2024-05-27 13:42:26 +0300

    esgfseg

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.filters.state import StateFilter
 from UserStates import UserStates
+from keyboard_helper import get_keyboard
 
 from config import TOKEN
 
@@ -15,7 +16,8 @@ dp=Dispatcher(storage=storage)
 
 @dp.message(Command("start"))
 async def start(message: types.Message, state:FSMContext):
-    await message.answer("ku trup")
+    kb=get_keyboard(["/test"])
+    await message.answer("ku trup", reply_markup=kb)
     await state.set_state(UserStates.BASE)
 
 @dp.message(Command("test"), StateFilter(UserStates.BASE))
